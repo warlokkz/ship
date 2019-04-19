9f044296140d88b80a9439fad3306f5ec1a75c92
warlokkz <alexander.anich@gmail.com>
2019-04-18 23:57:14 -0700

    trying to tie in NavMesh into the ECS system for moving around

---
+++ Assets/Scripts/NavigationSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Concurrent;
+using System.Numerics;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine.AI;
+
+namespace Ship.Project {
+    
+    [DisableAutoCreation]
+    public class NavigationSystem : JobComponentSystem
+    {
+        private struct AgentData
+        {
+            public int Index;
+            public Entity Entity;
+        }
+
+        private NativeQueue<AgentData> _needsWayPoint;
+        private ConcurrentDictionary<int, Vector3[]> _waypoints = new ConcurrentDictionary<int, Vector3[]>();
+        private NativeHashMap<int, AgentData> _pathFindingData;
+        
+        [BurstCompile]
+        private struct DetectNextWaypointJob : IJobParallelFor
+        {
+            public int NavMeshQuerySystemVersion;
+            public NativeQueue<AgentData>.Concurrent NeedsWayPoint;
+
+            public void Execute(int index)
+            {
+                
+            }
+        }
+
+        [BurstCompile]
+        private struct MovementJob : IJobParallelFor
+        {
+            
+            
+            public void Execute(int index)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+        
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
