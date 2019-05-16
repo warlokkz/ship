5d1e19636d947b3168884f3ae81405e47d21e2df
warlokkz <alexander.anich@gmail.com>
2019-05-15 23:43:38 -0700

    creating beginning of spawn system to generate a new track

---
+++ Assets/Scripts/Track/SpawnSystem.cs
@@ -0,0 +1,13 @@
+using Unity.Entities;
+
+namespace Ship.Project.Track
+{
+	[DisableAutoCreation]
+	public class SpawnSystem : ComponentSystem
+	{
+		protected override void OnUpdate()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
